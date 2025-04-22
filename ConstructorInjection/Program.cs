0feb78ad8ed36003a86319c10bbc902af38f7146
Mohammad Bakhtiari <mohammadba86@yahoo.com>
2025-04-22 18:53:10 +0330

    Modifying program

--- ConstructorInjection/Program.cs
+++ ConstructorInjection/Program.cs
@@ -1,34 +1,69 @@
+using DataLayer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using Service.UserServices;
 
-namespace ConstructorInjection
+namespace ConstructorInjection;
+
+public class Program
 {
-    public class Program
+    private static string GetSecret()
     {
-        public static void Main(string[] args)
+        var userSecretId = typeof(Program).Assembly
+                        .GetCustomAttributes(typeof(UserSecretsIdAttribute), false)
+                        .OfType<UserSecretsIdAttribute>()
+                        .FirstOrDefault()?.UserSecretsId;
+        if (userSecretId == null)
+            throw new Exception("Init Application Configuration File Has Not Set Yet!");
+        return userSecretId!;
+    }
+
+    private static void AddConnectionStrings(WebApplicationBuilder builder)
+    {
+        var userSecretId = GetSecret();
+        builder.Configuration.AddUserSecrets(userSecretId);
+
+        var masterConnectionString = builder.Configuration.GetConnectionString("MasterConnection");
+
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                                                         options.UseSqlServer(masterConnectionString)
+                                                         .EnableSensitiveDataLogging());
+    }
+
+    private static void RegisterServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<IUserReader, UserReader>();
+        builder.Services.AddScoped<IUserSaver, UserSaver>();
+        builder.Services.AddScoped<IUserRemover, UserRemover>();
+        builder.Services.AddScoped<IUserServices, UserServices>();
+    }
+
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        AddConnectionStrings(builder);
+
+        RegisterServices(builder);
+
+        builder.Services.AddControllers();
+
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            app.MapOpenApi();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
