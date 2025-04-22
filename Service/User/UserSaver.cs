92da37304f9a949359aea0b8f76f7bba1f9098dd
Mohammad Bakhtiari <mohammadba86@yahoo.com>
2025-04-22 18:54:29 +0330

    Adding user saver service

---
+++ Service/User/UserSaver.cs
@@ -0,0 +1,26 @@
+﻿using DataLayer;
+using Entity;
+
+namespace Service.UserServices;
+
+public interface IUserSaver
+{
+    Task<int> SaveUserAsync(AppUser user);
+}
+
+public class UserSaver : IUserSaver
+{
+    private readonly ApplicationDbContext dbContext;
+
+    public UserSaver(ApplicationDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public async Task<int> SaveUserAsync(AppUser user)
+    {
+        await dbContext.AddAsync(user);
+        await dbContext.SaveChangesAsync();
+        return user.Id;
+    }
+}
