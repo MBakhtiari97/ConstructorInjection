4d79ff8074715ea0e3d20aa8943cac85fe852383
Mohammad Bakhtiari <mohammadba86@yahoo.com>
2025-04-22 18:53:25 +0330

    Adding db context

---
+++ DataLayer/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+    {
+    }
+
+    public DbSet<AppUser> AppUser { get; set; }
+    public DbSet<UserAddress> UserAddress { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
