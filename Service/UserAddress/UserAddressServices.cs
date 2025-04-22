320140e2cfe9f0b3dbb3e43f1b395df5238f4f9e
Mohammad Bakhtiari <mohammadba86@yahoo.com>
2025-04-22 20:54:26 +0330

    Adding user address services

---
+++ Service/UserAddress/UserAddressServices.cs
@@ -0,0 +1,19 @@
+﻿namespace Service.UserAddressServices;
+
+public interface IUserAddressServices
+{
+    Task<int> DeleteUserAddressAsync(int userAddressId);
+}
+
+public class UserAddressServices : IUserAddressServices
+{
+    private readonly IUserAddressRemover _userAddressRemover;
+
+    public UserAddressServices(IUserAddressRemover userAddressRemover)
+    {
+        _userAddressRemover = userAddressRemover;
+    }
+
+    public async Task<int> DeleteUserAddressAsync(int userAddressId)
+        => await _userAddressRemover.DeleteUserAddressAsync(userAddressId);
+}
