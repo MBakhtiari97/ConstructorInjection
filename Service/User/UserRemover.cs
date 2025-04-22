7f2b8807c719f01ad513cbfc0924e6e0a51fae83
Mohammad Bakhtiari <mohammadba86@yahoo.com>
2025-04-22 18:54:21 +0330

    Adding user remover service

---
+++ Service/User/UserRemover.cs
@@ -0,0 +1,32 @@
+﻿using DataLayer;
+using Microsoft.EntityFrameworkCore;
+using System.Net.NetworkInformation;
+
+namespace Service.UserServices;
+
+public interface IUserRemover
+{
+    Task<int> DeleteUserAsync(int userId);
+}
+
+public class UserRemover : IUserRemover
+{
+    private readonly ApplicationDbContext dbContext;
+    private readonly IUserServices _userServices;
+
+    public UserRemover(ApplicationDbContext dbContext, IUserServices userServices)
+    {
+        this.dbContext = dbContext;
+        this._userServices = userServices;
+    }
+
+    public async Task<int> DeleteUserAsync(int userId)
+    {
+        var user = await _userServices.GetUserAsync(userId);
+        if (user == null)
+            throw new Exception("User not found");
+        dbContext.AppUser.Remove(user);
+        await dbContext.SaveChangesAsync();
+        return user.Id;
+    }
+}
