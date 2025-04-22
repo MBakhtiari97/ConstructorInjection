a4f7345a846bcbbf246e08787fba9aec0f7f8800
Mohammad Bakhtiari <mohammadba86@yahoo.com>
2025-04-22 20:54:16 +0330

    Adding user address remover

---
+++ Service/UserAddress/UserAddressRemover.cs
@@ -0,0 +1,28 @@
+﻿using DataLayer;
+
+namespace Service.UserAddressServices;
+
+public interface IUserAddressRemover
+{
+    Task<int> DeleteUserAddressAsync(int userAddressId);
+}
+
+public class UserAddressRemover : IUserAddressRemover
+{
+    private readonly ApplicationDbContext dbContext;
+
+    public UserAddressRemover(ApplicationDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public async Task<int> DeleteUserAddressAsync(int userAddressId)
+    {
+        var userAddress = await dbContext.UserAddress.FindAsync(userAddressId);
+        if (userAddress == null)
+            throw new Exception("User not found");
+        dbContext.UserAddress.Remove(userAddress);
+        await dbContext.SaveChangesAsync();
+        return userAddress.Id;
+    }
+}
