55a5e0e8ef8259e3235a9d5fe44bcb0392fb2f80
Mohammad Bakhtiari <mohammadba86@yahoo.com>
2025-04-22 18:54:13 +0330

    Adding user reader service

---
+++ Service/User/UserReader.cs
@@ -0,0 +1,33 @@
+﻿using DataLayer;
+using Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Service.UserServices;
+
+public interface IUserReader
+{
+    Task<AppUser?> GetUserAsync(int userId);
+    Task<IEnumerable<AppUser>?> GetAllUsersAsync();
+}
+
+public class UserReader : IUserReader
+{
+    private readonly ApplicationDbContext dbContext;
+
+    public UserReader(ApplicationDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public async Task<IEnumerable<AppUser>?> GetAllUsersAsync()
+    {
+        return await dbContext.AppUser
+        .ToListAsync();
+    }
+
+    public async Task<AppUser?> GetUserAsync(int userId)
+    {
+        return await dbContext.AppUser
+            .FindAsync(userId);
+    }
+}
